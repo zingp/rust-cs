59254293747ece82a72fd5ff03b70347d98fd214
zingp <605156398@qq.com>
2020-04-18 19:38:16 +0800

    hello

---
+++ hello-rust/target/debug/deps/hello_rust-45cf6a4f59c44138.d
@@ -0,0 +1,5 @@
+/Users/liuyouyuan/workspace/rustproject/rust-cs/hello-rust/target/debug/deps/hello_rust-45cf6a4f59c44138: src/main.rs
+
+/Users/liuyouyuan/workspace/rustproject/rust-cs/hello-rust/target/debug/deps/hello_rust-45cf6a4f59c44138.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/liuyouyuan/workspace/rustproject/rust-cs/hello-rust/target/debug/hello-rust: /Users/liuyouyuan/workspace/rustproject/rust-cs/hello-rust/src/main.rs
